4436f9fb492f6bc1bfc37272e85ba6158e3ef552
lsavoie <twosavoie@gmail.com>
2019-04-07 14:34:54 -0500

    finish tutorial

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ $('.header').height($(window).height());
+})
+
+$(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+
+ })
